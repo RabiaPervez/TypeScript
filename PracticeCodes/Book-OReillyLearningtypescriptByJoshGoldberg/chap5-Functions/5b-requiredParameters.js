5469238a51d2cb5fdd073f38d5416b157ea9e31f
RabiaPervez <biapervez15@gmail.com>
2022-12-08 22:29:30 +0500

    Delete 5b-requiredParameters.js

--- PracticeCodes/Book-OReillyLearningtypescriptByJoshGoldberg/chap5-Functions/5b-requiredParameters.js
+++
@@ -1,7 +0,0 @@
-function sing2(first, second) {
-    console.log("".concat(first, "  ").concat(second, " are singing!"));
-}
-;
-sing2('ballad', 'rock');
-//sing2('helo'); (will give error)
-//sing2('helo', 'ballad', 'rock'); (will give error)
