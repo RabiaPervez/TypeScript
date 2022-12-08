5ed6a1b36d8cf6b5ad7ee1c5847bb723af9c9b7b
RabiaPervez <biapervez15@gmail.com>
2022-12-08 22:29:01 +0500

    Delete 3f-TypeAliases.js

--- PracticeCodes/Book-OReillyLearningtypescriptByJoshGoldberg/chap3-UnionsAndLiterals/3f-TypeAliases.js
+++
@@ -1,7 +0,0 @@
-// Type aliases can be used to create a new type from an existing type
-var rawDataFirst;
-var rawDataSecond;
-var rawDataThird;
-//type aliases cannot be referred in runtime
-// type Sometype = string | boolean;
-//console.log(Sometype); (will give error)
