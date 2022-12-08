07fdb5b4b233ef9283d21d3d77ecccb0d71f93da
RabiaPervez <biapervez15@gmail.com>
2022-12-08 22:26:30 +0500

    Delete 4e-UsageChecking.js

--- PracticeCodes/Book-OReillyLearningtypescriptByJoshGoldberg/Chap4-Objects/4e-UsageChecking.js
+++
@@ -1,9 +0,0 @@
-var hasBoth1 = {
-    first: "Oliver",
-    last: "Twist"
-};
-/*
-const hasOnlyOne: FirstAndLastNames = {
-    first: "John",
-}; // it will giv error as it should have both values of first and last
-*/
