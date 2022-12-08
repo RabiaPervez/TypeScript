925bdd4dad97b63afced2aa325cd2143d723a688
RabiaPervez <biapervez15@gmail.com>
2022-12-08 22:30:59 +0500

    Delete 5m-FunctionTypeAlises2.js

--- PracticeCodes/Book-OReillyLearningtypescriptByJoshGoldberg/chap5-Functions/5m-FunctionTypeAlises2.js
+++
@@ -1,5 +0,0 @@
-function usesNumberToString(numberToString) {
-    console.log("The string is: ".concat(numberToString(1234)));
-}
-usesNumberToString(function (input) { return "".concat(input, "! Hooray!"); });
-//usesNumberToString((input) => input * 2); // (will give error)
